dc8f51482ecc6cb2ebac0efddcc37f20b0b56de3
CAndersonArts <connorandersonarts@gmail.com>
2016-03-22 16:22:44 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://CAArts:dbpass123@ds064748.mlab.com:64748/caarts2106'
+};
