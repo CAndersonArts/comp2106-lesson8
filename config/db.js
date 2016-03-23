e27453c2e9ac2a4210601de5a7ce8069a2369954
CAndersonArts <connorandersonarts@gmail.com>
2016-03-23 01:05:49 -0400

    changed the git callback url
    was localhost, now heroku app

--- config/db.js
+++ config/db.js
@@ -8,5 +8,5 @@ module.exports = {
     'url': 'mongodb://CAArts:dbpass123@ds064748.mlab.com:64748/caarts2106',
     'githubClientId': '54b6a1db8e60ce02656c',
     'githubClientSecret': '4547eebaf0836532a842cd34820f3b911356eb1c',
-    'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'
+    'githubCallbackUrl': 'http://https://comp2106-lesson8.herokuapp.com/auth/github/callback'
 };
